52de06b9da508e669faa7314d7e4208d8b44957f
Northman1227 <Tnorris1227@gmail.com>
2016-11-07 10:41:39 -0500

    Add files via upload

---
+++ Tnorrisassingment6submissionfolder/myscript.js
@@ -0,0 +1,28 @@
+<script>
+                
+function toggleheader() {
+                   
+ if (document.getElementById('contentHeader').style.display === 'block') 
+                     
+{document.getElementById('contentHeader').style.display = 'none';
+                    
+document.getElementsbyTagname('a')[0].innerHTML = 'Show Content';}
+                   
+ Else {document.getElementById('contentHeader').style.display = 'block';
+                    
+document.getElementsByTagName('a')[0}.innerHTML = 'Hide Content';}
+                    }
+                
+function popup() {alert("Just one of many features I can bring to your site!">;}
+    
+$("li").animate({left:100})
+$("#hide").click(function(){
+	$("li").each(function);
+	$("p").toggleclass(red);
+	$("p").hide();
+	$( "tbody" ).html( "<b>Wow!</b> I know I impress myself" );
+	$( "tbody b" )
+	});
+              
+           
+</script>
